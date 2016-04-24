9d24ce1703c3d454c0764e493b5d68733011bd6c
andrey-danilov <andrey-danilov.com@yandex.ua>
2016-04-24 17:01:18 +0300

    2

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,6 +1,7 @@
 import javafx.scene.shape.Ellipse;
 
 import java.awt.*;
+import javax.sound.sampled.Line;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
@@ -20,14 +21,17 @@ public class mainClass extends JPanel {
     static Line2D yLine;
 
     static JButton point= new JButton("add point");
-    static JTextField x = new JPasswordField(10);
-    static JTextField y= new JPasswordField(10);
-    static JButton line= new JButton("add line");
-    static JTextField x1 = new JPasswordField(10);
-    static JTextField y1= new JPasswordField(10);
-    static JTextField x2 = new JPasswordField(10);
-    static JTextField y2= new JPasswordField(10);
+    static JTextField x = new JTextField("0",10);
+    static JTextField y= new JTextField("0",10);
 
+    static Color mycolor = (Color.black);
+    static JButton line= new JButton("add line");
+    static JTextField x1 = new JTextField("0",10);
+    static JTextField y1= new JTextField("0",10);
+    static JTextField x2 = new JTextField("0",10);
+    static JTextField y2= new JTextField("0",10);
+
+    static JButton color= new JButton("set color");
 
     static ArrayList points;
     static ArrayList lines;
@@ -41,18 +45,16 @@ public class mainClass extends JPanel {
 
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(710, 700);
+        frame.setSize(700, 700);
         frame.setVisible(true);
-        xMain=frame.getSize().getWidth()/2;
-        yMain=frame.getSize().getHeight()/2;
 
-        pointMain= new Ellipse2D.Double(xMain-2.5,yMain-2.5,5,5);
-        xLine = new Line2D.Double(0,yMain,xMain*2,yMain);
-        yLine= new Line2D.Double(xMain,0,xMain,yMain*2);
+
+
+
 
 
         MyPanel panel = new MyPanel();
-        panel.setSize((int)xMain*2,(int)yMain*2);
+        panel.setLayout(null);
         panel.setLocation(0, 0);
         frame.add(panel);
     }
@@ -61,59 +63,83 @@ public class mainClass extends JPanel {
     {
 
         public MyPanel()
-        {   newPoint =null;
+        {   xMain=frame.getSize().getWidth()/2;
+            yMain=frame.getSize().getHeight()/2;
+
+            this.setSize((int)xMain*2,(int)yMain*2);
+            newPoint =null;
             newLine =null;
             points = new ArrayList();
             addMouseListener(new CustomListener());
             addMouseMotionListener(new MyMuve());
+            addMouseMotionListener(new mainMuve());
             lines = new ArrayList();
             point.setSize(100, 30); point.setLocation(0, 0);
             add(point);
             point.addActionListener(new Pointadd());
-            x.setEnabled(true);     x.setSize(50, 30);  x.setLocation(100, 0);
-            y.setEnabled(true);     y.setSize(50, 30);  y.setLocation(150, 0);
-            add(x, BorderLayout.NORTH);
-            add(y);
             add(point);
+            x.setEnabled(true);
+            x.setSize(50, 30);  x.setLocation(100, 0);
+            add(x);
+            y.setEnabled(true);
+            y.setSize(50, 30);  y.setLocation(150, 0);
+            add(y);
+
             line.setSize(100, 30); line.setLocation(0, 60);
             line.addActionListener(new Lineadd());
-            x1.setEnabled(true);     x1.setSize(50, 30);  x1.setLocation(100, 60);
-            y1.setEnabled(true);     y1.setSize(50, 30);  y1.setLocation(150, 60);
-            x2.setEnabled(true);     x2.setSize(50, 30);  x2.setLocation(100, 90);
-            y2.setEnabled(true);     y2.setSize(50, 30);  y2.setLocation(150, 90);
-            add(x1, BorderLayout.NORTH);
-            add(y1);
-            add(x2, BorderLayout.NORTH);
-            add(y2);
             add(line);
+            x1.setEnabled(true);
+            x1.setSize(50, 30);  x1.setLocation(100, 60);
+            add(x1);
+            y1.setEnabled(true);
+            y1.setSize(50, 30);  y1.setLocation(150, 60);
+            add(y1);
+            x2.setEnabled(true);
+            x2.setSize(50, 30);  x2.setLocation(100, 90);
+            add(x2);
+            y2.setEnabled(true);
+            y2.setSize(50, 30);  y2.setLocation(150, 90);
+            add(y2);
+
+            color.setSize(90, 30); color.setLocation(200, 0);
+            color.addActionListener(new setColor());
+            add(color);
             repaint();
         }
 
         public void paintComponent(Graphics g){
             super.paintComponent(g);
             g2=(Graphics2D)g;
+            pointMain= new Ellipse2D.Double(xMain-2.5,yMain-2.5,5,5);
+            xLine = new Line2D.Double(0,yMain,xMain*2,yMain);
+            yLine= new Line2D.Double(xMain,0,xMain,yMain*2);
             g2.setColor(Color.BLACK);
             g2.draw(yLine);
             g2.draw(xLine);
             g2.fill(pointMain);
+            g2.setColor(mycolor);
+            for(int i=0; i<points.size(); i++)
+            {
+                g2.fill((Ellipse2D) points.get(i));
+            }
+            for(int i=0; i<lines.size(); i++)
+            {
+
+                g2.draw((Line2D) lines.get(i));
+            }
 
 
-
-                for(int i=0; i<points.size(); i++)
-                   {
-                        g2.fill((Ellipse2D) points.get(i));
-                    }
             }
 
 
         public void add(Point point) {
-            newPoint= new Ellipse2D.Double(point.getX()-10,point.getY()-10,20,20);
+            newPoint= new Ellipse2D.Double(point.getX()-5,point.getY()-5,10,10);
             points.add(newPoint);
             repaint();
         }
         public void addLine(Point p1,Point p2) {
             newLine= new Line2D.Double(p1.getX(),p1.getY(),p2.getX(),p2.getY());
-            lines.add(newPoint);
+            lines.add(newLine);
             repaint();
         }
 
@@ -138,12 +164,13 @@ public class mainClass extends JPanel {
         public class CustomListener extends MouseAdapter {
 
             public void mouseClicked(MouseEvent e) {
+                if (!pointMain.contains(e.getPoint())){
                 if(e.getClickCount()>=2)
                 {
                     newPoint = find(e.getPoint());
                     if(newPoint!=null) remove(newPoint);
                 }
-            }
+            }}
 
             public void mouseEntered(MouseEvent e) {
 
@@ -154,10 +181,15 @@ public class mainClass extends JPanel {
             }
 
             public void mousePressed(MouseEvent e) {
-                newPoint = find(e.getPoint());
-                if(newPoint == null){ add(e.getPoint());
-                }}
+                if (!pointMain.contains(e.getPoint())){
+                    newPoint = find(e.getPoint());
+                if (newPoint == null) {
+                    add(e.getPoint());
+                }
+            }
 
+
+            }
             public void mouseReleased(MouseEvent e) {
 
             }
@@ -166,29 +198,24 @@ public class mainClass extends JPanel {
         {
             public void mouseMoved(MouseEvent e)
             {
-
             }
             public void mouseDragged(MouseEvent e)
-            {   if(xMain==e.getX() && yMain==e.getY())
-                    {
-                        remove(pointMain);
-                    }else {
-                            if(newPoint!=null)
-                                {
-                                newPoint.setFrame(e.getX()-10,e.getY()-10,20,20);
-                                repaint();
-                                }
-                    }
+            {if (!pointMain.contains(e.getPoint())){
+                if (newPoint != null) {
+                    newPoint.setFrame(e.getX() - 5, e.getY() - 5, 10, 10);
+                    repaint();
+                }
             }
 
 
+            }
         }
         public class Pointadd implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 int xt =new Integer(x.getText()).intValue();
                 int yt =new Integer(y.getText()).intValue();
 
-                Point temp = new Point(xt,yt);
+                Point temp = new Point((int)xMain+xt,(int)yMain+yt);
 
                 add(temp);
             }
@@ -201,10 +228,32 @@ public class mainClass extends JPanel {
                 int xt2 =new Integer(x2.getText()).intValue();
                 int yt2 =new Integer(y2.getText()).intValue();
 
-                Point x = new Point(xt1,yt1);
-                Point y = new Point(xt2,yt2);
+                Point x = new Point((int)xMain+xt1,(int)yMain+yt1);
+                Point y = new Point((int)xMain+xt2,(int)yMain-yt2);
                 addLine(x,y);
 
+            }
+        }
+
+        private class setColor implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+            mycolor= JColorChooser.showDialog(null,"set your color",mycolor);
+                repaint();
+
+            }
+        }
+
+
+        public class mainMuve implements MouseMotionListener {
+            public void mouseMoved(MouseEvent e)
+            {
+            }
+            public void mouseDragged(MouseEvent e)
+            {
+                if(pointMain.contains(e.getPoint())){
+                    xMain=e.getX(); yMain=e.getY();
+                    repaint();
+                }
             }
         }
     }
